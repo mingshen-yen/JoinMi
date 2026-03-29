9fde3200038fe2af57901e361134644e49bb6cf2
mingshen-yen <minghsuanyen@gmail.com>
2026-03-29 16:10:06 +0200

    setup api

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,13 +1,5 @@
 import "#db";
 import express from "express";
-import { errorHandler } from "#middleware";
-import {
-  artistRoutes,
-  eventRelationRoutes,
-  eventRoutes,
-  locationRoutes,
-  uploadRoutes,
-} from "#routes";
 import cors from "cors";
 
 const app = express();
@@ -25,16 +17,11 @@ app.use(express.json());
 app.route("/").get((req, res) => {
   res.json("Hello World");
 });
-app.use("/events", eventRoutes);
-app.use("/artists", artistRoutes);
-app.use("/locations", locationRoutes);
-app.use("/upload", uploadRoutes);
-app.use("/eventrelations/", eventRelationRoutes);
+
 app.use("*splat", (req, res) => {
   throw new Error("Not found", { cause: { status: 404 } });
 });
 
-app.use(errorHandler);
 app.listen(port, () =>
   console.log(`Server is running on port http://localhost:${port}`),
 );
